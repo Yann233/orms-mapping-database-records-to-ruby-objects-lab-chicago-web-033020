13db7e4459c8d86f55a0dc63d94fffe6784ce242
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-10 15:02:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,8 +23,7 @@ class Student
           FROM students;
         SQL
 
-        DB[:conn].execute(sql).map do |row|
-          self.new_from_db(row)
+        DB[:conn].execute(sql)
         end
       end
 
