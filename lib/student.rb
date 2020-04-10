1ccf683a3b5b1a4b758586b052ad2893fe93c894
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-10 15:09:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,6 +67,28 @@ class Student
    end
  end
 
+ def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+      SQL
+
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT 1
+      SQL
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
